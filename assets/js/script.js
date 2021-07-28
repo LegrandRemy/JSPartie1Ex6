06cfdb9255249c46aa2ca8ca28f9f87003d3066a
LegrandRemy <legrandremy19@outlook.fr>
2021-07-28 17:00:11 +0200

    corrigé exercice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
-let sendformulaire1 = document.getElementById('sdformulaire');
-sendformulaire1.addEventListener('click', sendformulaire);
+// Déclaration de variable de l'envoi du formulaire
+let sendFormButton = document.getElementById('sendForm');
+//Au clic sur le bouton appel de la fonction sendForm
+sendFormButton.addEventListener('click', sendForm);
+//Déclaration de la fonction
+function sendForm() {
+    //Récuperation des valeurs envoyées par l'utilisateur
+    let shoeSizeUser = document.getElementById('shoeSize').value;
+    let yearOfBirthUser = document.getElementById('yearOfBirth').value;
 
-function sendformulaire (){
-let shoesize1 = document.getElementById('shoesize').value;
-let birth1 = document.getElementById('birth').value;
-let result = shoesize1 * 2;
- result = result + 5;
- result = result * 50;
- result = result - birth1;
- result = result + 1769;
-alert(result);
+    // 1ere methode
+    let resultOneLine = (shoeSizeUser * 2 + 5) * 50 - yearOfBirthUser + 1771;
+    alert(resultOneLine);
+
+    // 2eme methode
+    let result = shoeSizeUser * 2;
+    result = result + 5;
+    result = result * 50;
+    result = result - yearOfBirthUser;
+    result = result + 1771;
+    alert(result);
+
+    // 3eme methode
+    let result2 = shoeSizeUser * 2;
+    result2 += 5;
+    result2 *= 50;
+    result2 -= yearOfBirthUser;
+    result2 += 1771;
+    alert(result2);
 }
